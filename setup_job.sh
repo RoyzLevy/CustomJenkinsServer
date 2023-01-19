8aa9106f3bde76aa1c353b2c4f3bb7782a05ac1e
Roy Levy <royalevy10@gmail.com>
2023-01-19 16:52:47 +0200

    copy files bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar
 
-if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
-    java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+
+if curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar; then
+    if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
+        java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+    fi
+else
+    echo "Something went wrong"
 fi
+echo "Done!"
+
+
