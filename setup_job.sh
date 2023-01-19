34f86277e27a14397906bb850f09cc81a233e875
Roy Levy <royalevy10@gmail.com>
2023-01-19 17:16:56 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar; then
-    echo "Installed jenkins cli"
-    if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
-        java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
-    fi
-else
-    echo "Something went wrong"
+if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
+    java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
 fi
-echo "Done!"
 
-
+# if curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar; then
+#     echo "Installed jenkins cli"
+#     if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
+#         java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+#     fi
+# else
+#     echo "Something went wrong"
+# fi
+# echo "Done!"
