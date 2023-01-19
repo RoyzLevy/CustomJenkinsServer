253cbfe7e5b861f6f7fe3aba8e3201a854c56125
Roy Levy <royalevy10@gmail.com>
2023-01-19 15:27:36 +0200

    copy files bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "authenticating as admin"
-echo "admin" | java -jar /usr/share/jenkins/cli.jar -s http://localhost:8080/ login --username admin --password-from-stdin
+# echo "authenticating as admin"
+# echo "admin" | java -jar /usr/share/jenkins/cli.jar -s http://localhost:8080/ login --username admin --password-from-stdin
 
 curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /usr/share/jenkins/cli.jar
 
-if ! java -jar jenkins-cli.jar -s http://localhost:8080/ get-job triggerpythonjob ; then 
-    java -jar jenkins-cli.jar -s http://localhost:8080/ create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+if ! java -jar /usr/share/jenkins/cli.jar -s http://localhost:8080/ get-job triggerpythonjob ; then 
+    java -jar /usr/share/jenkins/cli.jar -s http://localhost:8080/ create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
 fi
