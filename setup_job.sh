f459411a51fcd85db97201a12a44663270123434
Roy Levy <royalevy10@gmail.com>
2023-01-19 17:16:56 +0200

    jenkins cli bugfix and testing
    README.md updated

--- setup_job.sh
+++ setup_job.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar; then
-    echo "Installed jenkins cli"
-    if ! java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin get-job triggerpythonjob ; then 
-        java -jar /var/jenkins_home/cli.jar -s http://localhost:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
-    fi
-else
-    echo "Something went wrong"
-fi
-echo "Done!"
+# curl http://jenkins-server:8080/
 
+curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
 
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin list-plugins
+
+# cd $JENKINS_HOME/plugins
+# curl -O http://updates.jenkins-ci.org/download/plugins/cobertura.hpi
+
+# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin workflow-job
+# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
+
+# sleep 300
+# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin restart
+
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
