a1cc17961481ae01f9f4744d9a59d856613a335e
Roy Levy <royalevy10@gmail.com>
2023-01-20 16:01:34 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -4,11 +4,21 @@
 
 curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
 
-curl  -L https://updates.jenkins.io/update-center.json | sed '1d;$d' | curl -X POST -H 'Accept: application/json' -d @- http://jenkins-server:8080/updateCenter/byId/default/postBack
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin list-plugins
+
+# cd $JENKINS_HOME/plugins
+# curl -O http://updates.jenkins-ci.org/download/plugins/cobertura.hpi
+# curl http://yourservername:8080/reload
+
 
 java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
 
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+
+
+
+
+
 
 # if curl --silent --location http://localhost:8080/jnlpJars/jenkins-cli.jar -o /var/jenkins_home/cli.jar; then
 #     echo "Installed jenkins cli"
