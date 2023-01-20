a801007526a7895917fb4515bfd7e2fd3590db88
Roy Levy <royalevy10@gmail.com>
2023-01-20 16:04:28 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -8,10 +8,10 @@ java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth ad
 
 # cd $JENKINS_HOME/plugins
 # curl -O http://updates.jenkins-ci.org/download/plugins/cobertura.hpi
-# curl http://yourservername:8080/reload
 
 
 java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
+curl http://yourservername:8080/reload
 
 # java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
 
