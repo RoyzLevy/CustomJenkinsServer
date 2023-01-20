f49dde0b537e56c1ea99172a6c03de3f5bcf2298
Roy Levy <royalevy10@gmail.com>
2023-01-20 17:33:33 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -10,12 +10,9 @@ java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth ad
 # curl -O http://updates.jenkins-ci.org/download/plugins/cobertura.hpi
 
 # java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin workflow-job
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin email-ext
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin email-ext
+# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
 
 # sleep 300
 # java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin restart
 
-# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
