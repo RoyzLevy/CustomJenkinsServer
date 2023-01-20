c7661e6becb919647fd8c819f5d153190a716e2d
Roy Levy <royalevy10@gmail.com>
2023-01-20 13:16:30 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUN curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
+# RUN curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
 
 if ! java -jar /usr/local/bin/jenkins-cli.jar -s http://172.17.0.1:8080/ -auth admin:admin get-job triggerpythonjob ; then 
     java -jar /usr/local/bin/jenkins-cli.jar -s http://172.17.0.1:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
