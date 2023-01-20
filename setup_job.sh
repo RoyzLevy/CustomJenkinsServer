55bc2aa47fb4491f0bbee26ee222802c2cc1910a
Roy Levy <royalevy10@gmail.com>
2023-01-20 15:18:39 +0200

    jenkins cli bugfix

--- setup_job.sh
+++ setup_job.sh
@@ -4,7 +4,7 @@
 
 curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
 
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin pipeline-utility-steps
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin workflow-job@2.32
 
 java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
 
