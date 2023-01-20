1c9db9df559a48f788e90644ed1531b8b6aacbb8
Roy Levy <45877691+RoyzLevy@users.noreply.github.com>
2023-01-20 18:38:34 +0200

    Updated README.md
    clean code fixes
    
    trigger testing

--- setup_job.sh
+++ setup_job.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-# curl http://jenkins-server:8080/
-
 curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
-
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin list-plugins
-
-# cd $JENKINS_HOME/plugins
-# curl -O http://updates.jenkins-ci.org/download/plugins/cobertura.hpi
-
-# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin workflow-job
-# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin install-plugin git
-
-# sleep 300
-# java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin restart
-
 java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
+java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ build triggerpythonjob
