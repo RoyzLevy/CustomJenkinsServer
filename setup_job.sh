0ff24e3fba0708660fdb98ecae2de3e32b27b6fe
Roy Levy <royalevy10@gmail.com>
2023-01-21 16:56:42 +0200

    code clean for bash script

--- setup_job.sh
+++ setup_job.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-curl --silent --location http://jenkins-server:8080/jnlpJars/jenkins-cli.jar -o /usr/local/bin/jenkins-cli.jar
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ -auth admin:admin create-job triggerpythonjob < /var/jenkins_home/jobs/triggerpythonjob/config.xml && echo "Job created"
-java -jar /usr/local/bin/jenkins-cli.jar -s http://jenkins-server:8080/ build triggerpythonjob
+
+jenkins_url="http://jenkins-server:8080"
+jenkins_cli_jar_path="usr/local/bin/jenkins-cli.jar"
+python_job_config_path="/var/jenkins_home/jobs/triggerpythonjob/config.xml"
+
+# download the jenkins cli from the jenkins-server service
+curl --silent --location "${jenkins_url}/jnlpJars/jenkins-cli.jar" -o "${jenkins_cli_jar_path}"
+
+# create the trigger job based on the config file
+java -jar "${jenkins_cli_jar_path}" -s "${jenkins_url}" -auth admin:admin create-job triggerpythonjob < "${python_job_config_path}"
+
+# build the trigger job
+java -jar "${jenkins_cli_jar_path}" -s "${jenkins_url}" build triggerpythonjob
