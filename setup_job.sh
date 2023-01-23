e68095c9a522b97ea4c76aaaabf8b1d7b48d8a48
Roy Levy <45877691+RoyzLevy@users.noreply.github.com>
2023-01-23 20:19:39 +0200

    Update setup_job.sh

--- setup_job.sh
+++ setup_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-jenkins_url="http://jenkins-server:8080"
+jenkins_url="http://${3}:8080"
 jenkins_cli_jar_path="usr/local/bin/jenkins-cli.jar"
 python_job_config_path="/var/jenkins_home/jobs/triggerpythonjob/config.xml"
 
-# download the jenkins cli from the jenkins-server service
+# download the jenkins cli from the jenkins server service
 curl --silent --location "${jenkins_url}/jnlpJars/jenkins-cli.jar" -o "${jenkins_cli_jar_path}"
 
 # create the trigger job based on the config file
